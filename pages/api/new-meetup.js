762bfa0039042cda28ac43cf4b3d2e31aebad2db
tadams95 <tyrelleadams28@gmail.com>
2023-01-11 15:52:08 -0800

    wire up app to MongoDB

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,29 @@
+// /api/new-meetup
+// POST /api/new-meetup
+
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://tyrelle:uXwHToCzS703bPEa@cluster0.csas9qx.mongodb.net/?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({message: "Meetup inserted!"})
+  }
+}
+
+export default handler;
