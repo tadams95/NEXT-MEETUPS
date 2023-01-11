f08695e239fec5586a0b6862a56f4e1626531b42
tadams95 <tyrelleadams28@gmail.com>
2023-01-11 15:05:23 -0800

    add getStaticPaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,42 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  //fetch data for a single meetup
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcTrgD6vRqkRXIPLrjDeHsExf7jAoC6VesXSgCSZ5Gyr6lYkcmzcPQ0Ca6xA7GFrgaNL",
+        id: meetupId,
+        title: "A First Meetup",
+        address: "Some address 5, 12345 Boracay",
+        description: "This is a first meetup",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
