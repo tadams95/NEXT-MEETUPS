34107106ab10d664552e8e54b30a83af798e030b
tadams95 <tyrelleadams28@gmail.com>
2023-01-10 19:19:24 -0800

    Styling fixes, add NewMeetup form

---
+++ pages/new-meetup/index.js
@@ -0,0 +1,14 @@
+//our-domain/new-meetup
+
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+
+    function addMeetupHandler(enteredMeetupData) {
+        console.log(enteredMeetupData);
+    }
+
+    return <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+}
+
+export default NewMeetupPage;
