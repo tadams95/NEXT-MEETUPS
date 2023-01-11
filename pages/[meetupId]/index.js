5f270b274cfbb7055b0c88a66d43db0cf1d92b77
tadams95 <tyrelleadams28@gmail.com>
2023-01-11 13:14:05 -0800

    Add styling to detail pages

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="http://t0.gstatic.com/licensed-image?q=tbn:ANd9GcTrgD6vRqkRXIPLrjDeHsExf7jAoC6VesXSgCSZ5Gyr6lYkcmzcPQ0Ca6xA7GFrgaNL"
+      title="A First Meetup"
+      address="Some address 5, 12345 Boracay"
+      description="This is a first meetup"
+    />
+  );
+}
+
+export default MeetupDetails;
