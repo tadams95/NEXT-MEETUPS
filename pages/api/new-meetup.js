f06abc1a6566a207a0fe5f73ff58ca2781e38edd
tadams95 <tyrelleadams28@gmail.com>
2023-01-11 15:53:46 -0800

    wire up app to MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -22,7 +22,7 @@ async function handler(req, res) {
 
     client.close();
 
-    res.status(201).json({message: "Meetup inserted!"})
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
